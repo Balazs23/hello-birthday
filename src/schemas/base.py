682a9972cdc7c70400eb6696282568924b4ba3c9
Balazs Nagy <balazs@triomic.hu>
2022-10-09 18:36:10 +0000

    remove old code

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -1,6 +1,3 @@
-# from datetime import datetime
-
-# from odmantic.bson import BSON_TYPES_ENCODERS
 from pydantic.main import BaseModel
 
 
@@ -8,9 +5,3 @@ from pydantic.main import BaseModel
 class BaseSchema(BaseModel):
     class Config:
         orm_mode = True
-        # MongoDB
-        # allow_population_by_field_name = True
-        # json_encoders = {
-        #     datetime: lambda d: d.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-        #     **BSON_TYPES_ENCODERS,
-        # }
